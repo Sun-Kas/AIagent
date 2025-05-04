69ca0f5e666c8b24834c89435ed688f10f6bff3c
Mak <1344566881@qq.com>
2025-05-04 14:48:33 +0800

    Initial commit

---
+++ src/main/java/com/mak/aiagent/AIagentApplication.java
@@ -0,0 +1,13 @@
+package com.mak.aiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIagentApplication.class, args);
+	}
+
+}
