2eef5cc8d0c3070caf525f8d288647b6cdf5c2cd
Mak <1344566881@qq.com>
2025-05-04 20:43:40 +0800

    初始工程

--- src/main/java/com/mak/aiagent/controller/HealthController.java
+++ src/main/java/com/mak/aiagent/controller/HealthController.java
@@ -1,4 +1,14 @@
 package com.mak.aiagent.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/health")
 public class HealthController {
+    @GetMapping
+    public String health() {
+        return "OK";
+    }
 }
